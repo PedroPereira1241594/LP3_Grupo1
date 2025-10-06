66dc9ae7caa3e613af1c9cca86988e5497d62b54
Pedro Pereira <1241594@isep.ipp.pt>
2025-10-06 21:52:58 +0100

    [GITHUB] - Criação do Projeto

---
+++ src/main/java/com/example/lp3_grupo1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.lp3_grupo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
